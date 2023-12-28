7e58dbe039859ea880b1d0a8dbf3378e4f45deb3
mwallace98 <mattwallace140@gmail.com>
2023-12-27 20:09:18 -0500

    all taks complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -114,9 +114,23 @@ router.put('/:id', (req, res) => {
 })
 
 
-
-router.get('/:id/messages', (req, res) => {
-    
+router.get('/:id/messages', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+       if(!post){
+        res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+        }else{
+            const messages = await Post.findCommentById(req.params.id)
+            res.json(messages)
+        }
+    } catch (err){
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message
+        })
+    }
 })
 
 
